61556a29cbfee77d45de09104cc50725988a9e87
raviX007 <91694681+raviX007@users.noreply.github.com>
2024-03-28 14:33:54 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
-/** @type {import('next').NextConfig} */
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config; 
+  }
 };
 
 export default nextConfig;
